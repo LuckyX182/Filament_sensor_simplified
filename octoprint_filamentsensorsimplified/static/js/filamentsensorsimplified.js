2859ef426d4bf4f733d95a1d517ee9f2c6bba71a
luckyx182 <lmalatinsky@gmail.com>
2020-04-15 22:17:18 +0200

    removed unused things in javascript

--- octoprint_filamentsensorsimplified/static/js/filamentsensorsimplified.js
+++ octoprint_filamentsensorsimplified/static/js/filamentsensorsimplified.js
@@ -5,13 +5,6 @@ $(function() {
 		self.settingsViewModel = parameters[0];
 
 		self.autoClose = ko.observable();
-		self.enableSpeech = ko.observable();
-		self.speechVoice = ko.observable();
-		self.speechVolume = ko.observable();
-		self.speechPitch = ko.observable();
-		self.speechRate = ko.observable();
-		self.voices = ko.observableArray();
-		self.speechEnabledBrowser = ko.observable();
 		self.msgType = ko.observable();
 		self.msgTypes = ko.observableArray([{
 						name : 'Notice',
@@ -33,7 +26,7 @@ $(function() {
 
 		self.onDataUpdaterPluginMessage = function(plugin, data) {
             if (plugin != "filamentsensorsimplified") {
-				// console.log('Ignoring '+plugin);
+				console.log('Ignoring '+plugin);
                 return;
             }
 
@@ -47,50 +40,23 @@ $(function() {
 						hide: self.settingsViewModel.settings.plugins.filamentsensorsimplified.autoClose()
 						});
 				}
-				if(self.enableSpeech() && ('speechSynthesis' in window)){
-					var msg = new SpeechSynthesisUtterance(data.msg);
-					msg.volume = self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechVolume();
-					msg.pitch = self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechPitch();
-					msg.rate = self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechRate();
-					msg.voice = speechSynthesis.getVoices().filter(function(voice){return voice.name == self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechVoice(); })[0];
-					speechSynthesis.cancel();
-					speechSynthesis.speak(msg);
-				}
 			}
 		}
 
 		self.onBeforeBinding = function() {
             self.msgType(self.settingsViewModel.settings.plugins.filamentsensorsimplified.msgType());
             self.autoClose(self.settingsViewModel.settings.plugins.filamentsensorsimplified.autoClose());
-			self.enableSpeech(self.settingsViewModel.settings.plugins.filamentsensorsimplified.enableSpeech());
-			self.speechVoice(self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechVoice());
-			self.speechVolume(self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechVolume());
-			self.speechPitch(self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechPitch());
-			self.speechRate(self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechRate());
         }
 
 		self.onEventSettingsUpdated = function (payload) {
             self.msgType = self.settingsViewModel.settings.plugins.filamentsensorsimplified.msgType();
             self.autoClose = self.settingsViewModel.settings.plugins.filamentsensorsimplified.autoClose();
-			self.enableSpeech(self.settingsViewModel.settings.plugins.filamentsensorsimplified.enableSpeech());
-			self.speechVoice(self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechVoice());
-			self.speechVolume(self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechVolume());
-			self.speechPitch(self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechPitch());
-			self.speechRate(self.settingsViewModel.settings.plugins.filamentsensorsimplified.speechRate());
         }
 
 		self.testPopUp = function(data) {
 			self.onDataUpdaterPluginMessage("filamentsensorsimplified", {'msg':'Filament Sensor Simplified pop up message example.','type':'popup'});
 		}
 
-		speechSynthesis.onvoiceschanged = function(e) {
-			if (self.voices().length > 0)
-				return;
-			var voicenames = speechSynthesis.getVoices();
-			voicenames.forEach(function(voice, i) {
-				self.voices.push({'name':voice.name,'value':voice.name})
-				});
-		};
     }
 
     // This is how our plugin registers itself with the application, by adding some configuration
