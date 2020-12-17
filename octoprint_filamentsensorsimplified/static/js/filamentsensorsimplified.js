ee928ae335197cd00751d229e92fab0615f6c2a2
LazeMSS <github@jemi.dk>
2020-12-17 13:33:11 +0100

    UI improvements Fixed a couple of UI bugs and inline styles Made javascript logic to show warning when setting up bad configs

--- octoprint_filamentsensorsimplified/static/js/filamentsensorsimplified.js
+++ octoprint_filamentsensorsimplified/static/js/filamentsensorsimplified.js
@@ -24,6 +24,9 @@ $(function () {
         }
 
         self.testSensor = function () {
+            // Cleanup
+            $("#filamentsensorsimplified_settings_testResult").removeClass("text-warning text-error text-info text-success");
+            // Make api callback
             $.ajax({
                     url: "/api/plugin/filamentsensorsimplified",
                     type: "post",
@@ -32,51 +35,65 @@ $(function () {
                     headers: {"X-Api-Key": UI_API_KEY},
                     data: JSON.stringify({
                         "command": "testSensor",
-                        "pin": $("#pinInput").val(),
-                        "power": $("#powerInput").val(),
-                        "mode": $("#gpioMode").val(),
-                        "triggered": $("#triggeredInput").val()
+                        "pin": $("#filamentsensorsimplified_settings_pinInput").val(),
+                        "power": $("#filamentsensorsimplified_settings_powerInput").val(),
+                        "mode": $("#filamentsensorsimplified_settings_gpioMode").val(),
+                        "triggered": $("#filamentsensorsimplified_settings_triggeredInput").val()
                     }),
                     statusCode: {
                         500: function () {
-                            $("#sensor-test-result-text").css("color", "red");
-                            self.testSensorResult("OctoPrint experienced issue. Check octoprint.log for further info");
+                            $("#filamentsensorsimplified_settings_testResult").addClass("text-error");
+                            self.testSensorResult('<i class="fas fa-exclamation-triangle"></i> OctoPrint experienced issue. Check octoprint.log for further info');
                         },
                         555: function () {
-                            $("#sensor-test-result-text").css("color", "red");
-                            self.testSensorResult("This pin is currently used by others, choose other pin");
+                            $("#filamentsensorsimplified_settings_testResult").addClass("text-error");
+                            self.testSensorResult('<i class="fas fa-exclamation-triangle"></i> This pin is currently used by others, choose other pin');
                         },
                         556: function () {
-                            $("#sensor-test-result-text").css("color", "red");
-                            self.testSensorResult("That is power, ground or out of range pin, choose other pin");
+                            $("#filamentsensorsimplified_settings_testResult").addClass("text-error");
+                            self.testSensorResult('<i class="fas fa-exclamation-triangle"></i> That is power, ground or out of range pin, choose other pin');
                         }
                     },
                     error: function () {
-                        $("#sensor-test-result-text").css("color", "red");
-                        self.testSensorResult("There was an error :(");
+                        $("#filamentsensorsimplified_settings_testResult").addClass("text-error");
+                        self.testSensorResult('<i class="fas fa-exclamation-triangle"></i> There was an error :(');
                     },
                     success: function (result) {
                         if (result.triggered === true) {
-                            $("#sensor-test-result-text").css("color", "green");
-                            self.testSensorResult("Sensor detected filament!");
+                            $("#filamentsensorsimplified_settings_testResult").addClass("text-success");
+                            self.testSensorResult('<i class="fas fa-toggle-on"></i> Sensor detected filament!');
                         } else {
-                            $("#sensor-test-result-text").css("color", "red");
-                            self.testSensorResult("Sensor triggered!")
+                            $("#filamentsensorsimplified_settings_testResult").addClass("text-info");
+                            self.testSensorResult('<i class="fas fa-toggle-off"></i> Sensor triggered!')
                         }
                     }
                 }
             );
         }
+        self.checkWarningPullUp = function(){
+            var mode =  $('#filamentsensorsimplified_settings_gpioMode').val();
+            var pin =  $('#filamentsensorsimplified_settings_pinInput').val();
+            var sencon = $('#filamentsensorsimplified_settings_powerInput').val();
+            console.log(pin,mode, sencon);
+            if (
+                sencon == 1 && (
+                    (mode == 10 && (pin==3 || pin == 5))
+                    ||
+                    (mode == 11 && (pin == 2 || pin == 3))
+                )
+            ){
+                $('#filamentsensorsimplified_settings_pullupwarn').removeClass('hidden pulsAlert').addClass('pulsAlert');
+            }else{
+                $('#filamentsensorsimplified_settings_pullupwarn').addClass('hidden').removeClass('pulsAlert');
+            }
+        }
 
-        getDisabled = function (item) {
+        self.getDisabled = function (item) {
             $.ajax({
                 type: "GET",
                 dataType: "json",
                 url: "plugin/filamentsensorsimplified/disable",
                 success: function (result) {
-                    console.log("success");
-                    console.log(result.gpio_mode_disabled);
-                    console.log(result.printing);
                     self.gpio_mode_disabled(result.gpio_mode_disabled)
                     self.printing(result.printing)
                 }
@@ -85,7 +102,10 @@ $(function () {
 
         self.onSettingsShown = function () {
             self.testSensorResult("");
-            getDisabled();
+            self.getDisabled();
+             // Check for broken settings
+            $('#filamentsensorsimplified_settings_gpioMode, #filamentsensorsimplified_settings_pinInput, #filamentsensorsimplified_settings_powerInput').off('change.fsensor').on('change.fsensor',self.checkWarningPullUp);
+            $('#filamentsensorsimplified_settings_gpioMode').trigger('change.fsensor');
         }
     }
 
