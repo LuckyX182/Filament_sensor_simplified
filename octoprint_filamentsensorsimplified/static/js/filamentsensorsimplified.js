b83a2af76b3c6b38f151eef998ccbb4111c7ed8b
luckyx182 <lmalatinsky@gmail.com>
2021-01-08 12:47:05 +0100

    test button fix

--- octoprint_filamentsensorsimplified/static/js/filamentsensorsimplified.js
+++ octoprint_filamentsensorsimplified/static/js/filamentsensorsimplified.js
@@ -63,7 +63,7 @@ $(function () {
                     success: function (result) {
                         // triggered when open
                         if ($("#filamentsensorsimplified_settings_triggeredInput").val() === "0") {
-                            if (result.triggered === false) {
+                            if (result.triggered === true) {
                                 $("#filamentsensorsimplified_settings_testResult").addClass("text-success");
                                 self.testSensorResult('<i class="fas icon-ok fa-check"></i> Sensor detected filament!');
                             } else {
