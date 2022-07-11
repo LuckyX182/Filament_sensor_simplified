0754d8fd912e7943db2389c0b5ad571835bab6f6
luckyx182 <lmalatinsky@gmail.com>
2022-07-12 00:48:16 +0200

    version increase

--- octoprint_filamentsensorsimplified/__init__.py
+++ octoprint_filamentsensorsimplified/__init__.py
@@ -467,7 +467,7 @@ class Filament_sensor_simplifiedPlugin(octoprint.plugin.StartupPlugin,
 __plugin_pythoncompat__ = ">=2.7,<4"  # python 2 and 3
 
 __plugin_name__ = "Filament Sensor Simplified"
-__plugin_version__ = "0.3.2"
+__plugin_version__ = "0.3.3"
 
 
 def __plugin_check__():
